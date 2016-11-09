0d284469b86320870eb70e1050da3cf381def63b
Drew Miller <d.m.lespaul@gmail.com>
2016-11-09 09:17:49 -0800

    finished code, working on probability sections now

--- localization.py
+++ localization.py
@@ -2,6 +2,7 @@
 import random
 from math import *
 
+
 worldX = 10
 worldY = 10
 world = [worldX, worldY]
@@ -67,12 +68,13 @@ class robot:
     def senseClean(self):
         return self.calcDistances()
 
-    def sense(self):
+    def doSense(self):
         dist = self.calcDistances()
         for i, d in enumerate(dist):
-            d = d + (random.random() * self.__sense_noise)
+            d = d + ((random.random() - .5)* 2 * self.__sense_noise)
             dist[i] = d
 
+        self.sense = dist
         return dist
 
 
@@ -90,12 +92,19 @@ class robot:
     #measurement is the vector measured from the
     #original robot. The prob is the probability of the
     #robot existing there
+
+
     def measurement_prob(self, measurement):
-        self.__prob = 1.0
+        # calculates how likely a measurement should be
+        prob = 1.0;
+        for i in range(len(locations)):
+            dist = sqrt((self.__x - locations[i][0]) ** 2 + (self.__y - locations[i][1]) ** 2)
+            prob *= Gaussian(dist, self.__sense_noise, measurement[i])
 
-        for i,d in enumerate(self.sense()):
-            self.__prob *= normpdf(d, self.__sense_noise, measurement[i])
+        self.__prob = prob
+        return prob
 
+    def prob(self):
         return self.__prob
 
     def resample(self):
@@ -105,30 +114,18 @@ class robot:
             self.__y = random.random() * worldY
             self.__orientation = random.random() * 2 * pi
 
+def Gaussian(mu, sigma, x):
+    # calculates the probability of x for 1-dim Gaussian with mean mu and var. sigma
+    return exp(- ((mu - x) ** 2) / (sigma ** 2) / 2.0) / sqrt(2.0 * pi * (sigma ** 2))
 
-def normpdf(x, mean, sd):
-    var = float(sd)**2
-    denom = (2*pi*var)**.5
-    num = exp(-(float(x)-float(mean))**2/(2*var))
-    return num/denom
 
 s = robot()
+s.setNoise(.1, .1, .1)
+r = robot()
+distances = s.calcDistances()
 
 particleNumber = 1000
 p = []
 for i in range(particleNumber):
     p.append(robot())
     p[i].setNoise(0,0,1)
-
-dest = s.sense()
-
-def start():
-    run = False
-    while not run:
-        run = True
-        for d, i in enumerate(p):
-            prob = i.measurement_prob(dest)
-            if prob < .9:
-                i.resample()
-            else:
-                print(str(d) + " " + str(prob))
